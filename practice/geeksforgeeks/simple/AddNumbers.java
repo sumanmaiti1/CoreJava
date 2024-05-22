f661f6b788ba5dc255a73b10ce0e74f3cd3a3691
Suman Maiti <sumanmaiti1@gmail.com>
2024-05-23 01:11:14 +0530

    SM: commiting tutorial code snippet till 22-may-2024

---
+++ practice/geeksforgeeks/simple/AddNumbers.java
@@ -0,0 +1,19 @@
+//2. Write a Program in Java to Add two Numbers.
+
+package com.corejava.practice.geeksforgeeks.simple;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        try{
+            for(String s:args){
+                sum += Integer.parseInt(s);
+            }
+        }
+        catch(NumberFormatException e){
+            System.err.println("Invalid input given : " + e.toString());
+            System.exit(0);
+        }
+    System.out.println("The sum is :- " + sum);
+    }
+}
---
+++ practice/geeksforgeeks/simple/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+// 1. Write Hello World Program in Java
+
+package com.corejava.practice.geeksforgeeks.simple;
+
+class FirstJavaProgram{
+	public static void main(String[] args){
+		System.out.println("Jay shree Krishna");
+	}
+}
